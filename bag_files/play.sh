8873b34fe35d977aa0f4dc2f5e830d4125122218
salvamarce <salvatore.marcellini@gmail.com>
2021-08-25 16:13:52 +0200

    funzionante

--- bag_files/play.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rosbag play -l $1 
-rosrun image_transport republish compressed in:=/rs435d/color/image_raw raw out:=/camera/
--- bag_files/record_markers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rosbag record -o $1 /mavros/vision_pose/pose /rs435d/color/image_raw/compressed /rs435d/color/camera_info 
